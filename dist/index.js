00b72286e775637979321b14064ffabca69668e0
gogy9000 <goropashnyj9000@gmail.com>
2022-12-25 21:03:00 +0500

    create crud

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,90 @@
 "use strict";
-// const express = require('express')
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+let entityes = [{ id: '1', title: '1', description: 'd' }, { id: '2', title: '2', description: 'd' }, {
+        id: '3',
+        title: '3',
+        description: 's'
+    }, { id: '4', title: '4', description: 'f' }];
+const parserMiddleware = (0, body_parser_1.default)({});
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3003;
+app.use(parserMiddleware);
 app.get('/', (req, res) => {
-    res.send('Hello Wo rldaaa!');
+    res.send('Hello!');
+});
+app.get('/entity', (req, res) => {
+    let arrayQuery = Object.keys(req.query);
+    if (arrayQuery.length !== 0) {
+        const filteredEntityes = entityes.filter((entity) => {
+            return arrayQuery.reduce((totalResult, currenQuery) => {
+                // @ts-ignore
+                if (currenQuery in entity && entity[currenQuery] === req.query[currenQuery]) {
+                    return totalResult;
+                }
+                else {
+                    return false;
+                }
+            }, true);
+        });
+        res.send(filteredEntityes);
+    }
+    else {
+        res.send(entityes);
+    }
+});
+app.post('/entity', (req, res) => {
+    if (req.body.title) {
+        const newEntity = { id: Date.now().toString(), title: req.body.title.toString(), description: '' };
+        entityes.push(newEntity);
+        res.send(newEntity);
+    }
+    else {
+        res.status(404);
+        res.send({ error: 'title is required' });
+    }
+});
+app.get('/entity/:id', (req, res) => {
+    const item = entityes.find((item) => item.id === req.params.id);
+    if (item) {
+        res.send(item);
+    }
+    else {
+        res.status(404);
+        res.send({ error: 'entity not found' });
+    }
+});
+app.put('/entity/:id', (req, res) => {
+    const item = entityes.find((item) => item.id === req.params.id);
+    if (item) {
+        if (req.body.title) {
+            item.title = req.body.title.toString();
+            res.send(item);
+        }
+        else {
+            res.status(404);
+            res.send({ error: 'title is required' });
+        }
+    }
+    else {
+        res.status(404);
+        res.send({ error: 'entity not found' });
+    }
+});
+app.delete('/entity/:id', (req, res) => {
+    const item = entityes.find((item) => item.id === req.params.id);
+    if (item) {
+        entityes = entityes.filter((item) => item.id !== req.params.id);
+        res.send(item);
+    }
+    else {
+        res.status(404);
+        res.send({ error: 'entity not found' });
+    }
 });
 app.listen(port, () => {
     console.log(`Example appp listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,94 @@
-// const express = require('express')
+import express, {Request, Response} from "express"
+import bodyParser from "body-parser";
 
-import express,{Request,Response} from "express"
 
-
+let entityes = [{id: '1', title: '1', description: 'd'}, {id: '2', title: '2', description: 'd'}, {
+    id: '3',
+    title: '3',
+    description: 's'
+}, {id: '4', title: '4', description: 'f'}]
+const parserMiddleware = bodyParser({})
 const app = express()
-const port = process.env.PORT|| 3003
+const port = process.env.PORT || 3003
+app.use(parserMiddleware)
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Hello Wo rldaaa!')
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello!')
+})
+app.get('/entity', (req: Request, res: Response) => {
+    let arrayQuery = Object.keys(req.query)
+    if (arrayQuery.length !== 0) {
+        const filteredEntityes = entityes.filter((entity) => {
+            return arrayQuery.reduce((totalResult, currenQuery) => {
+                // @ts-ignore
+                if (currenQuery in entity && entity[currenQuery] === req.query[currenQuery]) {
+                    return totalResult
+                } else {
+                    return false
+                }
+            }, true)
+        })
+        res.send(filteredEntityes)
+    } else {
+        res.send(entityes)
+    }
+
+
+})
+
+app.post('/entity', (req: Request, res: Response) => {
+
+    if (req.body.title) {
+        const newEntity = {id: Date.now().toString(), title: req.body.title.toString(),description:''}
+        entityes.push(newEntity)
+        res.send(newEntity)
+    } else {
+        res.status(404)
+        res.send({error: 'title is required'})
+    }
+})
+app.get('/entity/:id', (req: Request, res: Response) => {
+
+    const item = entityes.find((item) => item.id === req.params.id)
+    if (item) {
+        res.send(item)
+    } else {
+        res.status(404)
+        res.send({error: 'entity not found'})
+    }
+})
+app.put('/entity/:id', (req: Request, res: Response) => {
+
+    const item = entityes.find((item) => item.id === req.params.id)
+    if (item) {
+        if (req.body.title) {
+            item.title = req.body.title.toString()
+            res.send(item)
+        } else {
+            res.status(404)
+            res.send({error: 'title is required'})
+        }
+
+    } else {
+        res.status(404)
+        res.send({error: 'entity not found'})
+    }
+
+})
+app.delete('/entity/:id', (req: Request, res: Response) => {
+
+    const item = entityes.find((item) => item.id === req.params.id)
+    if (item) {
+        entityes = entityes.filter((item) => item.id !== req.params.id)
+        res.send(item)
+    } else {
+        res.status(404)
+        res.send({error: 'entity not found'})
+    }
 })
 
 app.listen(port, () => {
-    console.log(`Example appp listening on port ${port}`)})
+    console.log(`Example appp listening on port ${port}`)
+})
 
 module.exports = app
